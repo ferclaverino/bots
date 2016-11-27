8ba0e11af5f1829507597a7ae676e49b9b462b7d
Fernando Claverino@gmail.com <fernando.claverino@gmail.com>
2016-11-26 21:20:56 -0300

    add basic controll

--- carGoF/manual/main.js
+++ carGoF/manual/main.js
@@ -1,61 +1,56 @@
 var five = require("johnny-five");
 var temporal = require("temporal");
+var keypress = require('keypress');
 
 var board = new five.Board({
-  port: "/dev/rfcomm0"
+  port: "/dev/rfcomm0",
+  repl: false
 });
+
+keypress(process.stdin);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
 
 board.on('ready', function() {
   var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
   var motorL = new five.Motor(configs.M2);
   var motorR = new five.Motor(configs.M1);
-
-  // set 1s interval
-  var interval = 1000;
-
   // set half speed
   var speed = 255/2;
 
-  temporal.queue([{
-    delay: interval,
-    task: function() {
+  process.stdin.on('keypress', function(ch, key) {
+    if (!key) {
+      return;
+    } else if (key.name === 'q') {
+      process.exit();
+    } else if (key.name === 'up') {
       // forward
       motorL.forward(speed);
       motorR.forward(speed);
-    }
-  }, {
-    delay: interval,
-    task: function() {
+    } else if (key.name === 'down') {
       // reverse
       motorL.reverse(speed);
       motorR.reverse(speed);
-    }
-  }, {
-    delay: interval,
-    task: function() {
+    } else if (key.name === 'left') {
       // left
       motorL.reverse(speed);
       motorR.forward(speed);
-    }
-  }, {
-    delay: interval,
-    task: function() {
+    } else if (key.name === 'right') {
       // right
       motorL.forward(speed);
       motorR.reverse(speed);
-    }
-  }, {
-    delay: interval,
-    task: function() {
+    } else if (key.name === 'space') {
       // stop
       motorL.stop();
       motorR.stop();
     }
-  }]);
+  });
 
   this.on("exit", function() {
     // stop
     motorL.stop();
     motorR.stop();
   });
+  
 });
