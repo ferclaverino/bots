1e78c7928d6757f852d612eac8141d4948b7c134
Fernando Claverino@gmail.com <fernando.claverino@gmail.com>
2016-12-24 14:14:16 -0300

    simplify

--- hello-tessel/lcd-weather.js
+++ hello-tessel/lcd-weather.js
@@ -16,28 +16,16 @@ board.on('ready', () => {
     pins: ['a2', 'a3', 'a4', 'a5', 'a6', 'a7'],
   });
 
-  getWeather((data) => displayWeather(lcd, data));
-
-  board.loop(1000 * 60, () => {
-    getWeather((data) => displayWeather(lcd, data));
-  });
-
-});
-
-function getWeather(callback) {
   weather.now(6693229, (err, data) => {
     if(err) console.log(err);
     else {
-      callback(data);
+      let degreeTemp = data.getDegreeTemp();
+      let temperature = `${degreeTemp.temp.toFixed(1)} ${degreeTemp.temp_min} - ${degreeTemp.temp_max}`;
+      let weather = data.values.weather[0].description;
+
+      lcd.cursor(0, 0).print(temperature);
+      lcd.cursor(1, 0).print(weather);
     }
   });
-}
 
-function displayWeather(lcd, data) {
-  let degreeTemp = data.getDegreeTemp();
-  let temperature = `${degreeTemp.temp.toFixed(1)} ${degreeTemp.temp_min} - ${degreeTemp.temp_max}`;
-  let weather = data.values.weather[0].description;
-
-  lcd.cursor(0, 0).print(temperature);
-  lcd.cursor(1, 0).print(weather);
-}
+});
